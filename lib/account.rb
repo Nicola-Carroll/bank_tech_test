edf7c78f478845006bd5a01fcd72f108b0efafd7
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-27 13:58:49 +0100

    deposit 100

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,17 @@
 class Account
-  def print_statement
-    p 'date || credit || debit || balance'
+  def initialize
+    @transaction_log = []
+  end
+
+  def deposit
+    @transaction_log << [Time.new.strftime('%d/%m/%Y'), 100]
+  end
+
+  def statement
+    statement = ['date || credit || debit || balance']
+    @transaction_log.each do |transaction|
+      statement.push("#{transaction[0]} || 100 || || 100")
+    end
+    statement.join("\n")
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,11 +3,25 @@ require 'account'
 describe Account do
   let(:account) { described_class.new }
   let(:blank_statement) { 'date || credit || debit || balance' }
+  let(:today) { Time.new.strftime('%d/%m/%Y') }
 
-  describe '#print_statement' do
-    it 'prints a blank statement initially' do
-      expect { account.print_statement }.to output(/#{blank_statement}$/)
-        .to_stdout
+  describe '#statement' do
+    it 'returns a blank statement initially' do
+      expect(account.statement).to eq blank_statement
+    end
+
+    describe '#deposit' do
+      let(:expected) do
+        [
+          'date || credit || debit || balance',
+          "#{today} || 100 || || 100"
+        ].join("\n")
+      end
+
+      it 'can print one transaction' do
+        account.deposit
+        expect(account.statement).to eq expected
+      end
     end
   end
 end
