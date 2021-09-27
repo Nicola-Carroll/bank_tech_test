690ba9ca4f9871c059ad69f593aa5f53b208f16f
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-27 14:34:48 +0100

    refactor

--- lib/account.rb
+++ lib/account.rb
@@ -10,10 +10,20 @@ class Account
   def statement
     statement = ['date || credit || debit || balance']
     @transaction_log.each_with_index do |transaction, index|
-      statement.push(
-        "#{transaction[0]} || #{transaction[1]} || || #{@transaction_log[0, index].sum { |date, amount| amount } + transaction[1]}"
-      )
+      statement.push(render_transaction(index))
     end
     statement.join("\n")
+  end
+
+  private
+
+  def account_balance_following_transaction(index)
+    transaction = @transaction_log[index]
+    @transaction_log[0, index].sum { |date, amount| amount } + transaction[1]
+  end
+
+  def render_transaction(index)
+    transaction = @transaction_log[index]
+    "#{transaction[0]} || #{transaction[1]} || || #{account_balance_following_transaction(index)}"
   end
 end
