3e7e4115305cbe7d604550560447ffd8836dfde4
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-27 14:28:32 +0100

    the deposit amount can vary

--- lib/account.rb
+++ lib/account.rb
@@ -3,14 +3,16 @@ class Account
     @transaction_log = []
   end
 
-  def deposit
-    @transaction_log << [Time.new.strftime('%d/%m/%Y'), 100]
+  def deposit(amount)
+    @transaction_log << [Time.new.strftime('%d/%m/%Y'), amount]
   end
 
   def statement
     statement = ['date || credit || debit || balance']
     @transaction_log.each_with_index do |transaction, index|
-      statement.push("#{transaction[0]} || 100 || || #{100 * (index + 1)}")
+      statement.push(
+        "#{transaction[0]} || #{transaction[1]} || || #{@transaction_log[0, index].sum { |date, amount| amount } + transaction[1]}"
+      )
     end
     statement.join("\n")
   end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,14 +10,14 @@ describe Account do
       expect(account.statement).to eq blank_statement
     end
 
-    context 'account owner has topped up' do
-      let(:expected_100) do
+    context 'account owner has topped' do
+      let(:expect_100) do
         [
           'date || credit || debit || balance',
           "#{today} || 100 || || 100"
         ].join("\n")
       end
-      let(:expected_300) do
+      let(:expect_300) do
         [
           'date || credit || debit || balance',
           "#{today} || 100 || || 100",
@@ -25,15 +25,30 @@ describe Account do
           "#{today} || 100 || || 300"
         ].join("\n")
       end
-
-      it 'can print a statement for transaction' do
-        account.deposit
-        expect(account.statement).to eq expected_100
+      let(:expect_795) do
+        [
+          'date || credit || debit || balance',
+          "#{today} || 150 || || 150",
+          "#{today} || 405 || || 555",
+          "#{today} || 240 || || 795"
+        ].join("\n")
       end
 
-      it 'can calculate an account balance from multiple deposits' do
-        3.times { account.deposit }
-        expect(account.statement).to eq expected_300
+      it 'can print a statement for transaction' do
+        account.deposit(100)
+        expect(account.statement).to eq expect_100
+      end
+
+      it 'can calculate an account balance from multiple deposits of 100' do
+        3.times { account.deposit(100) }
+        expect(account.statement).to eq expect_300
+      end
+
+      it 'can handle varying deposit amounts' do
+        account.deposit(150)
+        account.deposit(405)
+        account.deposit(240)
+        expect(account.statement).to eq expect_795
       end
     end
   end
