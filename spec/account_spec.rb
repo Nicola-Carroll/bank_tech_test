ae2de1adb2a5686c44415f9f5aa3ad5ed877710d
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-27 16:59:32 +0100

    refactor account spec to use a transaction log mock

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,12 +2,34 @@ require 'account'
 require 'transaction_log'
 
 describe Account do
-  let(:account) { described_class.new(transaction_log_class: TransactionLog) }
+  let(:transaction_log) { double :transaction_log }
+  let(:transaction_log_class) do
+    double :transaction_log_class, new: transaction_log
+  end
+  let(:account) do
+    described_class.new(transaction_log_class: transaction_log_class)
+  end
+
   let(:blank_statement) { 'date || credit || debit || balance' }
   let(:today) { Time.new.strftime('%d/%m/%Y') }
 
+  describe '#deposit' do
+    it 'records a deposit in the transaction log' do
+      expect(transaction_log).to receive(:record_deposit).with(100)
+      account.deposit(100)
+    end
+  end
+
+  describe '#withdraw' do
+    it 'records a withdrawal in the transaction log' do
+      expect(transaction_log).to receive(:record_withdrawal).with(100)
+      account.withdraw(100)
+    end
+  end
+
   describe '#statement' do
     it 'returns a blank statement initially' do
+      allow(transaction_log).to receive(:transactions).and_return([])
       expect(account.statement).to eq blank_statement
     end
 
@@ -36,19 +58,29 @@ describe Account do
       end
 
       it 'can print a statement for one deposit' do
-        account.deposit(100)
+        allow(transaction_log).to receive(:transactions).and_return(
+          [[today, 100]]
+        )
+        allow(transaction_log).to receive(:total_following_transaction)
+          .and_return(100)
         expect(account.statement).to eq expect_100
       end
 
-      it 'can calculate an account balance from multiple deposits of 100' do
-        3.times { account.deposit(100) }
+      it 'can calculate account balance from multiple deposits of 100' do
+        allow(transaction_log).to receive(:transactions).and_return(
+          [[today, 100], [today, 100], [today, 100]]
+        )
+        allow(transaction_log).to receive(:total_following_transaction)
+          .and_return(100, 200, 300)
         expect(account.statement).to eq expect_300
       end
 
       it 'can handle varying deposit amounts' do
-        account.deposit(150)
-        account.deposit(405)
-        account.deposit(240)
+        allow(transaction_log).to receive(:transactions).and_return(
+          [[today, 150], [today, 405], [today, 240]]
+        )
+        allow(transaction_log).to receive(:total_following_transaction)
+          .and_return(150, 555, 795)
         expect(account.statement).to eq expect_795
       end
     end
@@ -79,19 +111,29 @@ describe Account do
       end
 
       it 'can print a statement for one withdrawal' do
-        account.withdraw(100)
+        allow(transaction_log).to receive(:transactions).and_return(
+          [[today, -100]]
+        )
+        allow(transaction_log).to receive(:total_following_transaction)
+          .and_return(-100)
         expect(account.statement).to eq expect_negative_100
       end
 
       it 'can calculate an account balance from multiple withdrawals of 100' do
-        3.times { account.withdraw(100) }
+        allow(transaction_log).to receive(:transactions).and_return(
+          [[today, -100], [today, -100], [today, -100]]
+        )
+        allow(transaction_log).to receive(:total_following_transaction)
+          .and_return(-100, -200, -300)
         expect(account.statement).to eq expect_negative_300
       end
 
       it 'can combine deposits and withdrawals' do
-        account.deposit(150)
-        account.withdraw(40)
-        account.deposit(65)
+        allow(transaction_log).to receive(:transactions).and_return(
+          [[today, 150], [today, -40], [today, 65]]
+        )
+        allow(transaction_log).to receive(:total_following_transaction)
+          .and_return(150, 110, 175)
         expect(account.statement).to eq expect_175
       end
     end
